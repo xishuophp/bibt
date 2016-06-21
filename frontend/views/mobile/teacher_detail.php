d8890d86e71b618280f3302c7db337c67190ca58
gaea123 <gaea123@linmings-MacBook-Pro.local>
2016-06-21 08:35:35 +0800

    eeee

--- frontend/views/mobile/teacher_detail.php
+++ frontend/views/mobile/teacher_detail.php
@@ -6,4 +6,11 @@
 <h1 class="tit"><?php if($model) echo $model['real_name']; ?></h1>
 <div class="article">
 	<?php if($model) echo $model['details']; ?>
+    <div class="qrcode">
+        <h2 class="logo"><img src="/static/images/logo1.png"></h2>
+        <p class="t_name">北京经济技术职业学院</p>
+        <p>招生热线：010-61595668/61595685</p>
+        <p class="q_name">微信号：bibtwx</p>
+        <p class="p_qrcode"><img src="/static/images/qrcode.jpg"></p>
+    </div> 
 </div>

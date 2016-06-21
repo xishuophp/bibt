de7febb152089dbab27b72985de97fadb2bd3d64
gaea123 <gaea123@linmings-MacBook-Pro.local>
2016-06-21 08:22:27 +0800

    ddd

--- frontend/views/mobile/article_detail.php
+++ frontend/views/mobile/article_detail.php
@@ -5,6 +5,14 @@ use common\models\Utils;
 <?php if($model):?>
 <h1 class="tit"><?=$model['article_title']?></h1>
 <div class="article">
-    <?=$model['article_content']?> 
+    <?=$model['article_content']?>
+
+    <div class="qrcode">
+        <h2 class="logo"><img src="/static/images/logo1.png"></h2>
+        <p class="t_name">北京经济技术职业学院</p>
+        <p>招生热线：010-61595685</p>
+        <p class="q_name">微信号：bibtwx</p>
+        <p class="p_qrcode"><img src="/static/images/qrcode.jpg"></p>
+    </div> 
 </div>
 <?php endif;?>

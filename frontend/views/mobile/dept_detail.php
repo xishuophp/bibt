9d2a7ec09c326eccbd946efb95206e59738d8bb5
gaea123 <gaea123@linmings-MacBook-Pro.local>
2016-06-21 08:54:01 +0800

    444

--- frontend/views/mobile/dept_detail.php
+++ frontend/views/mobile/dept_detail.php
@@ -9,7 +9,7 @@
     <div class="qrcode">
         <h2 class="logo"><img src="/static/images/logo1.png"></h2>
         <p class="t_name">北京经济技术职业学院</p>
-        <p>招生热线：010-61595668/61595685</p>
+        <p>招生热线：010-61595668、010-61595685</p>
         <p class="q_name">微信号：bibtwx</p>
         <p class="p_qrcode"><img src="/static/images/qrcode.jpg"></p>
     </div> 

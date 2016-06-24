321f009c21ff70824e0b104ce9b4e8c0ab77f738
gaea123 <gaea123@linmings-MacBook-Pro.local>
2016-06-24 21:59:04 +0800

    ddd

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -32,9 +32,7 @@ AppAsset::register($this);
 
     <footer>
         <p>© 北京经济技术职业学院</p>
-        <span style="display:none;">
-        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1259670038'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1259670038' type='text/javascript'%3E%3C/script%3E"));</script>
-        </span>
+        
     </footer>
 
 </div>
